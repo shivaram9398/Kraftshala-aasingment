f3cdfa085dd4885e63a83fbcba06fb2ca46b0da1
shivaram9398 <palleshivaram24@gmail.com>
2024-06-16 22:33:16 +0530

    final commit

--- README.md
+++ README.md
@@ -1,70 +1,38 @@
-# Getting Started with Create React App
+# Weather App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This Weather App is a React-based application that provides real-time weather information for a specified city. It fetches weather data from the OpenWeatherMap API and displays it along with the current time for the location using the TimeZoneDB API. The application also supports dark mode.
 
-## Available Scripts
+## Technologies Used
 
-In the project directory, you can run:
+### Frontend
+- **React**: A JavaScript library for building user interfaces.
+- **React Icons**: A collection of popular icons for React applications.
+- **CSS**: Styling the application, including dark mode support.
 
-### `npm start`
+### APIs
+- **OpenWeatherMap API**: Provides current weather data for any location.
+- **TimeZoneDB API**: Provides time zone and current time data for any location based on latitude and longitude.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+### Hosting and Deployment
+- **Create React App**: A tool to set up a modern web app by running one command. It includes a development server, build scripts, and a set of default configurations.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## React Icons Used
 
-### `npm test`
+The application uses several icons from the `react-icons` library to enhance the UI:
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Getting Started
+To run this application locally, follow these steps:
 
-### `npm run build`
+## Clone the repository:
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## git clone https://github.com/your-repository/weather-app.git
+Navigate to the project directory:
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+cd weather-app
+## Install the dependencies:
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+npm install
+## Start the development server:
 
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+npm start
+The application will be available at http://localhost:3000.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
+import {Component} from "react"
 import './App.css';
+import { TiWeatherPartlySunny } from "react-icons/ti";
+import { TiLocationOutline } from "react-icons/ti";
+import { TiWeatherWindyCloudy } from "react-icons/ti";
+import { WiHumidity } from "react-icons/wi";
+import { MdDarkMode } from "react-icons/md";
+import { MdOutlineDarkMode } from "react-icons/md";
+import { FaTemperatureArrowDown } from "react-icons/fa6";
+import { FaTemperatureArrowUp } from "react-icons/fa6";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component {
+  state={weather:"",errormsg:"",time:[],inputValue:"Hyderabad",text:"",isdark:false}
+
+
+  gettime=async(data)=> {
+    const key="W7P8STJI47GO"
+    try {
+      const time=await fetch(`http://api.timezonedb.com/v2.1/get-time-zone?key=${key}&format=json&by=position&lat=${data.coord.lat}&lng=${data.coord.lon}`)
+    const res=await time.json()
+    this.setState({time:[res.cityName,res.countryName,res.formatted]})
+    } catch (error) {
+      this.setState({errormsg:error})
+    }
+  }
+  
+
+  getdatafromapi= async()=> {
+    const appid="fa469b89c2fd55fc3401f0a8f2f7af5f"
+    const {inputValue}=this.state
+    try {
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${appid}`
+    const response = await fetch(url)
+    const data = await response.json()
+    this.setState({text:data.weather.description})
+    this.setState({weather:[data.main.temp,data.main.temp_max,data.main.temp_min,data.main.humidity,data.wind.speed]}, ()=>this.gettime(data))
+
+    }catch (e) {
+      this.setState({errormsg:e})
+    }
+    this.setState({inputValue:""})
+  }
+
+  componentDidMount() {
+    this.getdatafromapi()
+  }
+
+  cahngeinput=(event)=> {
+    this.setState({inputValue:event.target.value})
+    
+  }
+  modeChange=()=> {
+    this.setState(prev=> ({isdark:!prev.isdark}))
+  }
+
+
+  render() {
+    const {weather,time,inputValue,isdark}=this.state
+    let num = weather[0]
+    let roundedNum = Math.round(num-273.15);
+    const darkmode=isdark ? "dark" : ""
+    const white=isdark ? "whitetext" : ""
+    
+
+    return (
+<>
+<div className={`main-container ${white}`}>
+<div className= {`container ${darkmode}`}>
+          <div className="header">
+            <h1 className="header-text">Current weather <span><button className="btn" onClick={this.modeChange}> {isdark ? <MdDarkMode style={{height:"20px",width:"20px"}} /> : <MdOutlineDarkMode style={{height:"20px",width:"20px"}}/>}</button></span></h1>
+            <h1 className="header-text">Time</h1>
+          </div>
+          <hr className="line"/>
+    <div className="search-container">
+              <div className="search-input">
+              <input type="search" placeholder="Enter City Name" className="search-box" onChange={this.cahngeinput} value={inputValue}/>
+              <button type="button" className="btn" onClick={this.getdatafromapi}>
+              <img src="https://img.freepik.com/premium-vector/search-map-vector-illustration-style_717774-29623.jpg?w=740" alt="img" height="20px" width="20px" />
+              </button>
+              </div>
+      <p><span><TiLocationOutline/></span>{time[0]},{time[1]}</p>
+      <p>{time[2]}</p>
     </div>
-  );
+                  <div className="cloud">
+                        <TiWeatherPartlySunny style={{ width: '150px', height: '150px',margin: "5px" }} />
+                        <h1 className="degree">{roundedNum}°</h1>
+                    </div>
+
+     <div className="bottom-container">
+      <div className="line-cloumn">
+                  <div className="left-con">
+            <div className="para"><FaTemperatureArrowUp /><p>Temp Max</p></div> 
+            <p>{weather[1]}</p>
+                  </div>
+                  <hr className="line"/>         
+        </div>
+
+        <div className="line-cloumn">
+                  <div className="left-con">
+            <div className="para"> <TiWeatherWindyCloudy /><p>wind speed</p></div> 
+            <p>{weather[4]}</p>
+                  </div>
+                  <hr className="line"/>         
+        </div>
+        <div className="line-cloumn">
+                  <div className="left-con">
+            <div className="para"><WiHumidity style={{height:"20px",width:"20px"}}/><p>Humidity</p></div> 
+            <p>{weather[3]}</p>
+                  </div>
+                  <hr className="line"/>         
+        </div>
+        <div className="line-cloumn">
+                  <div className="left-con">
+            <div className="para"><FaTemperatureArrowDown /><p>Temp Min</p></div> 
+            <p>{weather[2]}</p>
+                  </div>
+                  <hr className="line"/>         
+        </div>
+      </div>               
+    </div>
+</div>
+</>
+    )
+  }
 }
 
-export default App;
+export default App
